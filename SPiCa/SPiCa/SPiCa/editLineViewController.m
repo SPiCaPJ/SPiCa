7e01f0f8377bd749631ccf8b0ce4232b13c5e379
hochiai <rondofantasy17@yahoo.co.jp>
2014-10-30 22:14:13 +0900

    投稿のアラート

--- SPiCa/SPiCa/SPiCa/editLineViewController.m
+++ SPiCa/SPiCa/SPiCa/editLineViewController.m
@@ -13,6 +13,8 @@
 //ベースとなる画像
 UIImageView *showImageView;
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -49,8 +51,46 @@ UIImageView *showImageView;
     
 }
 
+// cancelボタンが押された時の処理
+- (void)cancel
+{
+    return;
+}
 
-- (IBAction)actionsocial:(id)sender {
+// saveボタンが押された時の処理
+- (void)save
+{
+    ////保存する画像を指定
+    //UIImage *image = [UIImage imageNamed:_imageName];
+    //画像を保存する
+    //UIImageWriteToSavedPhotosAlbum(showImageView, self, NULL, NULL);
+    
+    
+    //次のアラート
+    UIAlertController *alert2 =
+    [UIAlertController alertControllerWithTitle:@"完了"
+                                        message:@"画像を保存しました、SNSへ投稿しますか？" preferredStyle:UIAlertControllerStyleAlert];
+    [alert2 addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                               style:UIAlertActionStyleDestructive
+                                             handler:^(UIAlertAction *action) {
+                                                 NSLog(@"Cancel pushed");
+                                                 [self cancel];
+                                             }]];
+    
+    [alert2 addAction:[UIAlertAction actionWithTitle:@"OK"
+                                               style:UIAlertActionStyleDefault
+                                             handler:^(UIAlertAction *action) {
+                                                 NSLog(@"OK pushed");
+                                                 [self activ];
+                                                 
+                                             }]];
+    
+    [self presentViewController:alert2 animated:YES completion:nil];
+}
+
+// activボタンが押された時の処理
+- (void)activ
+{
     //投稿するテキスト
     NSString *sharedText = @"テキスト #SPiCa";
     //投稿するコンテンツ、ここに画像を記載
@@ -68,7 +108,36 @@ UIImageView *showImageView;
                                            appActivivities];
     //表示
     [self presentViewController:activityVC animated:YES completion:nil];
+}
+
+//完了ボタン
+- (IBAction)actionsocial:(id)sender {
     
+    //最初のアラート
+    UIAlertController *alert1 =
+    [UIAlertController alertControllerWithTitle:@"確認"
+                                        message:@"加工を終了して、画像を保存しますか？" preferredStyle:UIAlertControllerStyleAlert];
+    
+    //押されたときのハンドラ
+    [alert1 addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                              style:UIAlertActionStyleDestructive
+                                            handler:^(UIAlertAction *action) {
+                                          
+                                                
+                                                NSLog(@"Cancel pushed");
+                                                 [self cancel];
+                                            }]];
+    [alert1 addAction:[UIAlertAction actionWithTitle:@"OK"
+                                               style:UIAlertActionStyleDefault
+                                             handler:^(UIAlertAction *action) {
+                                                 NSLog(@"OK pushed");
+                                                 [self save];
+                                             }]];
+    
+    //アラート表示
+    [self presentViewController:alert1 animated:YES completion:nil];
+    
+
     }
 @end
 
